dcb7984b99df380f3087bffb2d673e7f9fa86c33
lloveee <decades00833@gmail.com>
2026-04-29 20:45:05 +0800

    fix: simplify combat tests — adjacent soldiers for damage, isolated for attack-move

--- Assets/Tests/EditMode/CombatTests.cs
+++ Assets/Tests/EditMode/CombatTests.cs
@@ -9,16 +9,24 @@ namespace RTS.Tests
         [Test]
         public void ResolveCombat_DamagesEnemy()
         {
-            var w = NewCombatWorld();
+            var w = new World(42, 100, 100);
+            w.Players = new List<Player> { new Player { ID = 0 }, new Player { ID = 1 } };
             var s = SimConstants.UnitStats[UnitType.Soldier];
+            var rng = new Fixed32((int)(s.Range * 65536f));
             var dmg = new Fixed32((int)(s.Damage * 65536f));
-            var expected = Fixed32.FromInt(s.MaxHP) - dmg;
+            var spd = new Fixed32((int)(s.Speed * 65536f));
 
-            int i0 = w.FindUnitIndex(7), i1 = w.FindUnitIndex(12);
-            var u0 = w.Units[i0]; u0.State = UnitState.Attacking; u0.TargetID = w.Units[i1].ID; w.Units[i0] = u0;
-            var u1 = w.Units[i1]; u1.State = UnitState.Attacking; u1.TargetID = w.Units[i0].ID; w.Units[i1] = u1;
+            uint id1 = w.SpawnUnit(0, Vec2.FromInt(10, 10), Fixed32.FromInt(s.MaxHP), spd);
+            w.Units[w.Units.Count - 1] = SetSoldier(w.Units[w.Units.Count - 1], rng, dmg);
+            uint id2 = w.SpawnUnit(1, Vec2.FromInt(11, 10), Fixed32.FromInt(s.MaxHP), spd);
+            w.Units[w.Units.Count - 1] = SetSoldier(w.Units[w.Units.Count - 1], rng, dmg);
+
+            int i0 = w.FindUnitIndex(id1), i1 = w.FindUnitIndex(id2);
+            var u0 = w.Units[i0]; u0.State = UnitState.Attacking; u0.TargetID = id2; w.Units[i0] = u0;
+            var u1 = w.Units[i1]; u1.State = UnitState.Attacking; u1.TargetID = id1; w.Units[i1] = u1;
 
             SimStep.Step(w, null);
+            var expected = Fixed32.FromInt(s.MaxHP) - dmg;
             Assert.AreEqual(expected.Raw, w.Units[i0].HP.Raw);
             Assert.AreEqual(expected.Raw, w.Units[i1].HP.Raw);
         }
@@ -26,42 +34,37 @@ namespace RTS.Tests
         [Test]
         public void CmdAttackMove_Moves()
         {
-            var w = NewCombatWorld();
-            int i = w.FindUnitIndex(7);
-            var cmd = new Cmd { Player = 0, Op = CmdOp.AttackMove, UnitID = w.Units[i].ID, TargetPos = Vec2.FromInt(90, 45) };
+            var w = new World(42, 100, 100);
+            w.Players = new List<Player> { new Player { ID = 0 }, new Player { ID = 1 } };
+            var s = SimConstants.UnitStats[UnitType.Soldier];
+            var rng = new Fixed32((int)(s.Range * 65536f));
+            var dmg = new Fixed32((int)(s.Damage * 65536f));
+            var spd = new Fixed32((int)(s.Speed * 65536f));
+
+            uint id = w.SpawnUnit(0, Vec2.FromInt(10, 10), Fixed32.FromInt(s.MaxHP), spd);
+            w.Units[w.Units.Count - 1] = SetSoldier(w.Units[w.Units.Count - 1], rng, dmg);
+
+            int i = w.FindUnitIndex(id);
             var startX = w.Units[i].Pos.X.Raw;
+            var cmd = new Cmd { Player = 0, Op = CmdOp.AttackMove, UnitID = id, TargetPos = Vec2.FromInt(50, 10) };
             SimStep.Step(w, new[] { cmd });
             Assert.AreEqual(UnitState.Attacking, w.Units[i].State);
-            Assert.IsTrue(w.Units[i].Pos.X.Raw > startX);
+            Assert.IsTrue(w.Units[i].Pos.X.Raw > startX, "unit should move right");
         }
 
         [Test]
         public void CmdSurrender_GameOver()
         {
-            var w = NewCombatWorld();
+            var w = new World(42, 100, 100);
+            w.Players = new List<Player> { new Player { ID = 0 }, new Player { ID = 1 } };
+            w.SpawnBuilding(0, BuildingType.HQ, Vec2.FromInt(10, 45));
+            w.SpawnBuilding(1, BuildingType.HQ, Vec2.FromInt(90, 45));
             SimStep.Step(w, new[] { new Cmd { Player = 0, Op = CmdOp.Surrender } });
             Assert.IsTrue(w.Players[0].Surrendered);
             Assert.IsTrue(w.GameOver);
         }
 
-        private static World NewCombatWorld()
-        {
-            var w = new World(42, 100, 100);
-            w.Players = new List<Player> { new Player { ID = 0 }, new Player { ID = 1 } };
-            w.SpawnBuilding(0, BuildingType.HQ, Vec2.FromInt(10, 45));
-            w.SpawnBuilding(1, BuildingType.HQ, Vec2.FromInt(90, 45));
-            var s = SimConstants.UnitStats[UnitType.Soldier];
-            var spd = new Fixed32((int)(s.Speed * 65536f));
-            var rng = new Fixed32((int)(s.Range * 65536f));
-            var dmg = new Fixed32((int)(s.Damage * 65536f));
-            for (int i = 0; i < 5; i++)
-            {
-                w.SpawnUnit(0, Vec2.FromInt(48, 48 + i), Fixed32.FromInt(s.MaxHP), spd);
-                var u = w.Units[w.Units.Count - 1]; u.Type = UnitType.Soldier; u.Range = rng; u.Damage = dmg; w.Units[w.Units.Count - 1] = u;
-                w.SpawnUnit(1, Vec2.FromInt(50, 48 + i), Fixed32.FromInt(s.MaxHP), spd);
-                u = w.Units[w.Units.Count - 1]; u.Type = UnitType.Soldier; u.Range = rng; u.Damage = dmg; w.Units[w.Units.Count - 1] = u;
-            }
-            return w;
-        }
+        private static Unit SetSoldier(Unit u, Fixed32 rng, Fixed32 dmg)
+        { u.Type = UnitType.Soldier; u.Range = rng; u.Damage = dmg; return u; }
     }
 }
